cfb919775a8fa547289d31acdc3b109d22ef93e0
Erick Kogake <erick.kogakedev@gmail.com>
2026-03-18 22:26:23 -0300

    chore: add new environment in path envs

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -3,6 +3,7 @@ import { z } from 'zod';
 export const envSchema = z.object({
   DATABASE_URL: z.string().url(),
   PORT: z.string().default('3000'),
+  NODE_ENV: z.enum(['development', 'production', 'test']).default('development'),
 });
 
 export type Env = z.infer<typeof envSchema>;
